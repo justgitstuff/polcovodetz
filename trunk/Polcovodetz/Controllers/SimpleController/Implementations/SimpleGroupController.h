12193547943ef2753f2b5299432fe1aad149e0d2
imanushin@mail.ru <imanushin@mail.ru@5b272218-22b4-11de-8691-b5df825c884c>
2009-04-07 08:34:41 +0000

    Добавлены реализации для простейших контролеров

---
+++ trunk/Polcovodetz/Controllers/SimpleController/Implementations/SimpleGroupController.h
@@ -0,0 +1,57 @@
+﻿
+
+#ifndef  __SIMPLE_GROUP_CONTROLLER
+#define  __SIMPLE_GROUP_CONTROLLER
+
+//-------------------------------------------------------
+
+#include <IGroupController.h>
+#include <boost/shared_ptr.hpp>
+
+//-------------------------------------------------------
+
+struct SimpleGroupControllerImpl;
+
+struct GroupInputMessage;
+struct GroupOutputMessage;
+
+//-------------------------------------------------------
+
+/**
+    Реализация простейшего IGroupController.
+
+    Реализация самая простейшая, так как класс планируется как образец для 
+    написания/тестирование других классов этого уровня.
+*/
+class SimpleGroupController : public IGroupController
+{
+    Q_OBJECT;
+
+public:
+    SimpleGroupController();
+
+    virtual QString name()const;
+    virtual QString description()const;
+
+    virtual bool    init( IGroupInputDriver*, IGroupOutputDriver* );
+
+signals:
+    void outputMessage( GroupOutputMessage* mesage );
+
+private:    
+    /**
+    Прячем часть полей внутрь cpp-шника.
+    */
+    boost::shared_ptr< SimpleGroupControllerImpl > m_impl;
+
+private slots:
+    void inputMessage( GroupInputMessage* mesage );
+
+};
+
+//-------------------------------------------------------
+
+#endif //__SIMPLE_GROUP_CONTROLLER
+
+//-------------------------------------------------------
+
---
+++ trunk/Polcovodetz/Controllers/SimpleController/Implementations/SimpleObjectController.h
@@ -0,0 +1,57 @@
+﻿
+
+#ifndef  __SIMPLE_OBJECT_CONTROLLER
+#define  __SIMPLE_OBJECT_CONTROLLER
+
+//-------------------------------------------------------
+
+#include <IObjectController.h>
+#include <boost/shared_ptr.hpp>
+
+//-------------------------------------------------------
+
+struct SimpleObjectControllerImpl;
+
+struct ObjectInputMessage;
+struct ObjectOutputMessage;
+
+//-------------------------------------------------------
+
+/**
+    Реализация простейшего IObjectController.
+
+    Реализация самая простейшая, так как класс планируется как образец для 
+    написания/тестирование других классов этого уровня.
+*/
+class SimpleObjectController : public IObjectController
+{
+    Q_OBJECT;
+
+public:
+    SimpleObjectController();
+
+    virtual QString name()const;
+    virtual QString description()const;
+
+    virtual bool    init( IObjectInputDriver*, IObjectOutputDriver* );
+
+signals:
+    void outputMessage( ObjectOutputMessage* mesage );
+
+private:    
+    /**
+    Прячем часть полей внутрь cpp-шника.
+    */
+    boost::shared_ptr< SimpleObjectControllerImpl > m_impl;
+
+private slots:
+    void inputMessage( ObjectInputMessage* mesage );
+
+};
+
+//-------------------------------------------------------
+
+#endif //__SIMPLE_OBJECT_CONTROLLER
+
+//-------------------------------------------------------
+
