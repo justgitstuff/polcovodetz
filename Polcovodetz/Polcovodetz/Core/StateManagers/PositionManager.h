9bdbe65d6a7b98c8dd28b969750747a51dee7fcc
imanushin <imanushin@mail.ru>
2009-10-06 17:32:41 +0000

    Добавлен класс: PositionManager: наблюдатель за позициями объектов.

---
+++ Polcovodetz/Polcovodetz/Core/StateManagers/PositionManager.h
@@ -0,0 +1,25 @@
+#ifndef POSITIONMANAGER_H
+#define POSITIONMANAGER_H
+
+//-------------------------------------------------------
+
+#include <PObject.h>
+
+//-------------------------------------------------------
+
+class PositionManager
+{
+public:
+    PositionManager();
+    ~PositionManager();
+
+private:
+    
+};
+
+//-------------------------------------------------------
+
+#endif // POSITIONMANAGER_H
+
+//-------------------------------------------------------
+
