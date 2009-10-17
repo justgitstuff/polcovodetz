2fea6d9588ceaedf420c2b7fa0242a278fa43e23
imanushin@mail.ru <imanushin@mail.ru@5b272218-22b4-11de-8691-b5df825c884c>
2009-10-17 17:15:13 +0000

    Добавлен класс: состояния группы.

---
+++ trunk/Polcovodetz/Polcovodetz/Core/BaseClasses/GroupStates.h
@@ -0,0 +1,21 @@
+
+#ifndef  __GROUP_STATES
+#define  __GROUP_STATES
+
+//-------------------------------------------------------
+
+namespace GroupState
+{
+    enum GroupState
+    {
+        GoAttack,
+        MakeDeffence
+    }
+}
+
+//-------------------------------------------------------
+
+#endif //__GROUP_STATES
+
+//-------------------------------------------------------
+
